1da3b64d70c7109b227aa795645109b035cb76c7
Andrea Lepidio <l3p@pm.me>
2020-10-02 16:26:23 +0200

    Initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
 export * from './lib/async';
-export * from './lib/hash';
+export * from './lib/vanity';
 export * from './lib/number';
+
+import vorpal from 'vorpal';
+
+import { getVanityWallet } from './lib/vanity';
+
+vorpal()
+  .delimiter(`vanity-eth2 】`)
+  .show()
+  .command('gen <input> [checksum] [suffix]', 'Generates an Ethereum wallet with public address matching input regex')
+  .action((args, cb) => {
+    const input = String(args.input);
+    const isChecksum = !!args.checksum;
+    const isSuffix = !!args.suffix;
+    console.log(input, isChecksum, isSuffix);
+    getVanityWallet(input, isChecksum, isSuffix, cb)
+  })
+

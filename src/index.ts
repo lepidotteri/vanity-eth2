9527c3ca9ff4c1aceba6067a847f1c1c17c7d4fb
Andrea Lepidio <l3p@pm.me>
2020-10-04 15:35:21 +0200

    Improved multithreading support with possible heap overflow management

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ export * from './lib/vanity';
 export * from './lib/number';
 
 import cluster from 'cluster';
+import v8 from 'v8';
 
+import os from 'os-utils';
 // eslint-disable-next-line import/order
 import Conf from 'conf';
 
@@ -11,11 +13,10 @@ import Conf from 'conf';
 import vorpal from 'vorpal';
 const Vorpal = vorpal();
 const chalk = Vorpal.chalk;
-Vorpal.delimiter(chalk.blue(`vanity-eth2 】`)).show();
 
 import { getVanityWallet } from './lib/vanity';
 
-const workers = [];
+const workers: number[] = [];
 /**
  * Setup number of worker processes to share port which will be defined while setting up server
  */
@@ -29,13 +30,13 @@ const setupWorkerProcesses = () => {
   // current example will utilize all of them
   for(let i = 0; i < numCores; i++) {
     // creating workers and pushing reference in an array
-    // these references can be used to receive messages from workers
-    workers.push(cluster.fork());
+    // these references can be used to receive messages from workers (and to remove them)
+    workers.push(cluster.fork().id);
 
     // to receive messages from worker process
-    workers[i].on('message', (message) => {
-      console.log(message);
-    });
+    // workers[i].on('message', (message) => {
+    //  console.log(message);
+    // });
   }
 
   // process is clustered on a core and process id is assigned
@@ -44,16 +45,20 @@ const setupWorkerProcesses = () => {
   });
 
   // if any of the worker process dies then start a new one by simply forking another one
-  // cluster.on('exit', (worker, code, signal) => {
-  //   Vorpal.log(chalk.cyan('Worker ' + worker.process.pid + ' died with code: ' + chalk.red(code) + ', and signal: ' + chalk.green(signal)));
-  //   Vorpal.log(chalk.cyan('Starting a new worker'));
-  //   cluster.fork();
-  //   workers.push(cluster.fork());
-  //   // to receive messages from worker process
-  //   workers[workers.length - 1].on('message', (message) => {
-  //     Vorpal.log(chalk.yellow.italic(`Worker ${process.pid} `) + message);
-  //   });
-  // });
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  cluster.on('exit', (worker, code, signal) => {
+    // Vorpal.log(chalk.cyan('Worker ' + worker.process.pid + ' died with code: ' + chalk.red(code) + ', and signal: ' + chalk.green(signal)));
+    // Vorpal.log(chalk.cyan('Starting a new worker'));
+    const oldWorker = workers.indexOf(worker.id);
+    workers.splice(oldWorker, 1);
+    const newWorker = cluster.fork();
+    workers.push(newWorker.id);
+    // to receive messages from worker process
+    // workers[workers.length - 1].on('message', (message) => {
+    // Vorpal.log(chalk.yellow.italic(`Worker ${process.pid} `) + message);
+    // });
+  });
 };
 
 let input, isChecksum, isSuffix;
@@ -63,9 +68,12 @@ const runResult = new Conf({ configName: 'runResult', cwd: process.cwd() });
 const saveAndShutdown = (wallet) => {
   runResult.set('wallet', wallet);
   runResult.set('timestamp', new Date().toISOString())
-  Vorpal.ui.redraw.done();
+  Vorpal.log(wallet);
   for (const id in cluster.workers) {
     workers[id].kill();
+  }
+  if(cluster.isMaster) {
+    process.exit(0);
   }
 }
 
@@ -78,6 +86,8 @@ const invokeCLI = (isClusterRequired) => {
   // if it is a master process then call setting up worker process
   if(isClusterRequired && cluster.isMaster) {
     Vorpal
+      .delimiter(chalk.blue(`vanity-eth2 】`))
+      .show()
       .command('gen <input> [checksum] [suffix]', 'Generates an Ethereum wallet with public address matching input regex')
       .action((args, cb) => {
         input = String(args.input);
@@ -87,10 +97,14 @@ const invokeCLI = (isClusterRequired) => {
         runConfig.set('input.isChecksum', isChecksum);
         runConfig.set('input.isSuffix', isSuffix);
         cb('Configured');
-        Vorpal.ui.redraw.done();
         setupWorkerProcesses();
+        // log CPU usage
+        // setTimeout(() => {
+        //   os.cpuUsage((v) => Vorpal.ui.redraw(chalk.green('CPU Usage: ' + v + '%' )))
+        // }, 500);
       })
   } else {
+
     input = runConfig.get('input.hex');
     isChecksum = runConfig.get('input.isChecksum');
     isSuffix = runConfig.get('input.isSuffix');
@@ -99,7 +113,19 @@ const invokeCLI = (isClusterRequired) => {
       throw Error('runConfig is missing');
     }
 
-    Vorpal.log(chalk.yellow.italic(`Worker: ${process.pid}`));
+    const initialStats = v8.getHeapStatistics();
+    const totalHeapSizeThreshold = initialStats.heap_size_limit * 95 / 100;
+    // console.log("totalHeapSizeThreshold: " + totalHeapSizeThreshold);
+
+    const detectHeapOverflow = () => {
+      const stats = v8.getHeapStatistics();
+      // console.log("total_heap_size: " + (stats.total_heap_size));
+      if ((stats.total_heap_size) > totalHeapSizeThreshold) {
+        process.exit();
+      }
+    };
+
+    setInterval(detectHeapOverflow, 1000);
     getVanityWallet(input, isChecksum, isSuffix,(wallet) => saveAndShutdown(wallet))
   }
 };
